9167e9739d481c72d809bc53531bb1f1965de450
Pedro Sotero <pedrupping@outlook.com>
2023-06-07 21:32:04 -0300

    retirei os typehints, estava dando problema no windows

--- interface.py
+++ interface.py
@@ -111,7 +111,7 @@ class TelaInicial(ctk.CTk):
 
 
 class TelaCadastro(ctk.CTkFrame):
-    def __init__(self, master: any, width: int = 200, height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | Tuple[str, str] = "transparent", fg_color: str | Tuple[str, str] | None = None, border_color: str | Tuple[str, str] | None = None, background_corner_colors: Tuple[str | Tuple[str, str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
+    def __init__(self, master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs):
         super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
 
         self.grid_columnconfigure(index=(0, 1, 2), weight=1)
@@ -131,7 +131,7 @@ class TelaCadastro(ctk.CTkFrame):
 
 
 class TelaAssociacao(ctk.CTkFrame):
-    def __init__(self, master: any, width: int = 200, height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | Tuple[str, str] = "transparent", fg_color: str | Tuple[str, str] | None = None, border_color: str | Tuple[str, str] | None = None, background_corner_colors: Tuple[str | Tuple[str, str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
+    def __init__(self, master, width = 200, height = 200, corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, background_corner_colors = None, overwrite_preferred_drawing_method = None, **kwargs):
         super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
 
         self.grid_columnconfigure(index=(0, 1, 2, 3, 4), weight=1)
